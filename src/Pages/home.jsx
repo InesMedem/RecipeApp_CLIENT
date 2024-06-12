8945bb6060598360139aadb84264bcd848214764
InesMedem <inesmedem93@gmail.com>
2024-06-12 16:26:48 +0200

    home update

--- src/Pages/home.jsx
+++ src/Pages/home.jsx
@@ -1,5 +1,59 @@
-const Home = () => {
-  return <div>home</div>;
+import axios from "axios";
+import { useEffect, useState } from "react";
+import useGetUserId from "../hooks/useGetUserId";
+
+export const Home = () => {
+  const [recipes, setRecipes] = useState([]);
+  const [savedRecipes, setSavedRecipes] = useState([]);
+
+  useEffect(() => {
+    const fetchRecipe = async () => {
+      try {
+        const response = await axios.get("http://localhost:3001/recipes");
+        setRecipes(response.data);
+        console.log(response.data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    fetchRecipe();
+  }, []);
+
+  const saveRecipe = async (recipeID) => {
+    const userID = useGetUserId();
+    try {
+      const response = await axios.put("http://localhost:3001/recipes", {
+        recipeID,
+        userID,
+      });
+      console.log(response);
+      setSavedRecipes(response.data.savedRecipes);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Recipes</h1>
+      <ul>
+        {recipes.map((recipe) => (
+          <li key={recipe._id}>
+            <div>
+              <h2>{recipe.name}</h2>
+              <button onClick={() => saveRecipe(recipe._id)}>Save </button>
+            </div>
+            <div className="instructions">
+              <p>{recipe.instructions}</p>
+            </div>
+            <img src={recipe.imageUrl} alt={recipe.name} />
+            <p> cooking Time: {recipe.cookingTime}(in munites)</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Home;
